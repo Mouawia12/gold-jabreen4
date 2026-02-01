f143908c2a5e3998bafeb7d4cbe260a55acb2950
mouawia <ymouawia10@gmail.com>
2026-02-01 09:55:04 +0100

    Describe your changes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Branch;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
@@ -14,11 +15,25 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $branch = Branch::first();
+        if (!$branch) {
+            $branch = Branch::create([
+                'branch_name' => 'Main Branch',
+                'branch_phone' => null,
+                'branch_address' => null,
+                'status' => 1,
+            ]);
+        }
+
         User::updateOrCreate(
             ['email' => 'info@admin.com'],
             [
                 'name' => 'Default Admin',
                 'password' => Hash::make('Rr$123#'),
+                'branch_id' => $branch->id,
+                'phone_number' => '0000000000',
+                'profile_pic' => '',
+                'role_name' => 'Admin',
                 'status' => 1,
             ]
         );
